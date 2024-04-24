7ad90e0c9448645f18918d089755e0a5f6787c55
ALI ALI <alirafikali22@gmail.com>
2024-04-24 04:07:34 +0300

    fix bugs

--- resources/views/product/creat.blade.php
+++ resources/views/product/creat.blade.php
@@ -4,16 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create Category</title>
+    <title>Create product</title>
 </head>
 <body>
 
-<form action='{{route('categories.store')}}' method='post'>
+<form action='{{route('products.store')}}' method='post'>
     @csrf
     <div>
        
             <input type='text' name='name' placeholder="Name" required>
-            <input type='submit'  value='Add Category'>
+            <input type='interger' name='quintity' placeholder="Quintity" required>
+        
+            <input type='submit'  value='Add product'>
     </div>
 
 </form>
